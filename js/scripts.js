1f751137a3b6efbd76c0f971475bb2d710d22442
Lawrence Otieno <lawrence.otieno@student.moringaschool.com>
2021-07-25 21:08:00 +0300

    portfolio css update

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
-// $(document).ready(function(){
-//     $(".oneDesign").click(function(){
-//         $("#wwd1").toggle();
-//         $("#oneDes").toggle();
-//     });
-//     $(".twoDevelopment").click(function(){
-//         $("#wwd2").toggle();
-//         $("#twoDev").toggle();
-//     });
-//     $(".threeProductMG").click(function(){
-//         $("#wwd3").toggle();
-//         $("#threeProd").toggle();
-//     });
-// });
+// WHAT WE DO
+$(document).ready(function(){
+    $(".oneDesign").click(function(){
+        $("#wwd1").toggle();
+        $("#oneDes").toggle();
+    });
+    $(".twoDevelopment").click(function(){
+        $("#wwd2").toggle();
+        $("#twoDev").toggle();
+    });
+    $(".threeProductMG").click(function(){
+        $("#wwd3").toggle();
+        $("#threeProd").toggle();
+    });
+});
 
 
 
@@ -76,14 +77,15 @@ $(document).ready(function(){
 })
 
 
-// function contactForm(){
-//     var name=document.getElementById('name').value;
-//     var email=document.getElementById('email').value;
-//     var message=document.getElementById('message').value;
+// CONTACT FORM
+function contactForm(){
+    var name=document.getElementById('name').value;
+    var email=document.getElementById('email').value;
+    var message=document.getElementById('message').value;
 
-//     if (name !== "" && email !== "" && message !== ""){
-//         alert(name+ ' we have received your message. Thank you for reaching out to us.');
-//     } else if(name !== "" || email !== "" || message !== ""){
-//         alert('Fill everything, please.')
-//     }
-// };
+    if (name !== "" && email !== "" && message !== ""){
+        alert(name+ ' we have received your message. Thank you for reaching out to us.');
+    } else if(name !== "" || email !== "" || message !== ""){
+        alert('Fill everything, please.')
+    }
+};
