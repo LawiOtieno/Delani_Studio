cbdf4d51dcf556ae7fdd26746d3992a95b3a7a95
Lawrence Otieno <lawrence.otieno@student.moringaschool.com>
2021-07-25 18:42:45 +0300

    form pop-up message

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,31 @@
-$(document).ready(function(){
-    $(".oneDesign").click(function(){
-        $("#wwd1").toggle();
-        $("#oneDes").toggle();
-    });
-    $(".twoDevelopment").click(function(){
-        $("#wwd2").toggle();
-        $("#twoDev").toggle();
-    });
-    $(".threeProductMG").click(function(){
-        $("#wwd3").toggle();
-        $("#threeProd").toggle();
-    });
-});
+// $(document).ready(function(){
+//     $(".oneDesign").click(function(){
+//         $("#wwd1").toggle();
+//         $("#oneDes").toggle();
+//     });
+//     $(".twoDevelopment").click(function(){
+//         $("#wwd2").toggle();
+//         $("#twoDev").toggle();
+//     });
+//     $(".threeProductMG").click(function(){
+//         $("#wwd3").toggle();
+//         $("#threeProd").toggle();
+//     });
+// });
+
+
+
+
+
+
+function contactForm(){
+    var name=document.getElementById('name').value;
+    var email=document.getElementById('email').value;
+    var message=document.getElementById('message').value;
+
+    if (name !== "" && email !== "" && message !== ""){
+        alert(name+ ' we have received your message. Thank you for reaching out to us.');
+    } else if(name !== "" || email !== "" || message !== ""){
+        alert('Fill everything, please.')
+    }
+};
